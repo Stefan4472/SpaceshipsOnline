7bfb4dedf157d5d7e56f9a60b662653b2540668f
DJStefan <stefan.kussmaul@gmail.com>
2018-11-14 07:33:14 -0500

    Spritesheet & SpritesheetPlayer Classes

---
+++ js/spritesheet.js
@@ -0,0 +1,14 @@
+/*
+Defines a spritesheet (one row of frames) using a texture_id value,
+the number of frames in the spritesheet, and the duration per frame.
+*/
+class Spritesheet {
+  constructor(img_id, texture_atlas, num_frames, ms_per_frame, loops) {
+    this.img_id = img_id;
+    this.num_frames = num_frames;
+    this.ms_per_frame = ms_per_frame;
+    this.loops = loops;
+    this.frame_width = texture_atlas.getWidth(img_id) / num_frames;
+    this.frame_height = texture_atlas.getHeight(img_id);
+  }
+}
