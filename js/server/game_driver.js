17d5a33fc25bb45bb363d32cb6f3a42a6ee3e0b4
DJStefan <stefan.kussmaul@gmail.com>
2018-12-06 12:20:09 -0500

    Work Rewriting Server Logic in GameDriver

--- js/server/game_driver.js
+++ js/server/game_driver.js
@@ -7,16 +7,142 @@ var Spaceship = require('./../shared/spaceship.js').Spaceship;
 var AmmoDrop = require('./../shared/ammo_drop.js').AmmoDrop;
 
 /*
+Defined GameMode types.
+*/
+var GameMode = {};
+GameMode.UNDEFINED = 0;
+GameMode.FREE_FOR_ALL = 1;
+
+/*
 Runs the game server-side. Meant to be sub-classed to implement a specific
 game mode.
 */
 class Game {
   constructor() {
+    console.log("Creating Game Instance");
+
     this.texture_atlas = new TextureAtlas();
-    this.sprite = new Sprite(0, 2, 100, 100, this.texture_atlas);
-    this.spaceship = new Spaceship(1, 100, 100, this.texture_atlas);
-    this.drop = new AmmoDrop(2, 100, 100, this.texture_atlas);
+
+    this.map_width = 1000;
+    this.map_height = 1000;
+
+    this.game_mode = GameMode.UNDEFINED;
+
+    // id of last connected player
+    this.last_player_id = 0;
+
+    // connected players
+    // each has a player_id, socket, username, score, kills, deaths
+    this.players = [];
+    // connected socket instances
+    this.sockets = [];
+    // spaceships controlled by players
+    // id corresponds to player_id controlling it
+    this.spaceships = [];
+    // bullets that have been fired by the spaceships
+    this.bullets = [];
+    // power_ups in the game
+    this.power_ups = [];
+
+    this.last_update_time = 0;
+
+    this.input_buffer = [];
+
+    this.started = false;
+
+    this.interval_id = 0;
+  }
+
+  start() {
+
+  }
+
+  stop() {
+
+  }
+
+  update() {
+
+  }
+
+  // attempts to add a player to the game
+  // passes in the socket the player is using to connect
+  addPlayer(socket) {
+    // initialize
+    var player_id = this.last_player_id++;
+    var x = this.randomInt(100, this.map_width - 100);
+    var y = this.randomInt(100, this.map_height - 100);
+    var heading = Math.random() * 2 * Math.PI;
+
+    console.log("Game adding player with " + player_id);
+
+    // create Spaceship instance and add to list
+    var ship = new Spaceship(player_id, x, y, this.texture_atlas);
+    ship.r_heading = heading;
+    ship.r_img_rotation = heading;
+    this.spaceships.push(ship);
+
+    // create player instance and add to list
+    var player = {
+      id: player_id,
+      socket: socket,
+      username: socket.username,
+      score: 0,
+      kills: 0,
+      deaths: 0
+    };
+
+    this.players.push(player);
+
+    // add socket instance to list
+    this.sockets.push({socket: socket, player_id: player_id});
+
+    // broadcast new player to other sockets
+    socket.broadcast.emit('player_joined', {id: player.id,
+      username: player.username, x: ship.x, y: ship.y,
+      r_heading: ship.r_heading});
+
+    // register control_input callback: add to control buffer
+    socket.on('control_input', function(data) {
+      this.queueInput({
+        player_id: player_id,
+        up_pressed: data.up_pressed,
+        down_pressed: data.down_pressed,
+        left_pressed: data.left_pressed,
+        right_pressed: data.right_pressed,
+        space_pressed: data.space_pressed
+      });
+    });
+
+    // register disconnect callback: remove player
+    socket.on('disconnect', function() {
+      // broadcast player_disconnect signal to other sockets TODO: ONLY SOCKETS IN THIS GAME
+      socket.broadcast.emit('player_disconnect', player_id);
+
+      this.removePlayer(player_id);
+    })
+  }
+
+  // removes player from the game
+  removePlayer(id) {  // TODO
+    console.log("Game removing player " + id);
+    for (var i = 0; i < this.players.length; i++) {
+      if (this.players[i].id == id) {
+        this.players[i].destroy = true;
+      }
+    }
+  }
+
+  // should have player_id, up/down/left/right/space pressed fields
+  queueInput(player_input) {
+    console.log("Queueing Input");
+    this.input_buffer.push(player_input);
+  }
+
+  randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
   }
 }
 
-module.exports = Game;
+module.exports.Game = Game;
+module.exports.GameMode = GameMode;
--- server.js
+++ server.js
@@ -6,10 +6,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-
-// require game code
-var game_driver = require('./js/server/game_driver');
-console.log(new game_driver().sprite.x);
+var game_driver = require('./js/server/game_driver.js');
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
@@ -28,60 +25,28 @@ server.listen(process.env.PORT || 8081, function() {
   console.log('Listening on '+ server.address().port);
 });
 
+// create the game instance
+var game_instance = new game_driver.Game();
+
+var num_connections = 0;
+
 // handle socket connection
 io.on('connection', function(socket) {
+
+  num_connections++;
+
   // 'newplayer' command: create data for the new player's state
   socket.on('new_player_request', function() {
-    socket.player = {
-      id: server.lastPlayderID++,
-      x: server.lastPlayderID * 10, //randomInt(100, 900),
-      y: server.lastPlayderID * 10 //randomInt(100, 900)
-    };
 
-    console.log("New Player Request. Assigned id " + socket.player.id);
+    // TODO: GET USERNAME FROM DATABASE OR SOMETHING
+    socket.username = 'Guest-' + num_connections.toString();
 
-    // send array of all connected players to the new socket
-    socket.emit('all_players', { players: getAllPlayers(), your_id: socket.player.id });
-    // broadcast the new player to all connections
-    socket.broadcast.emit('player_joined', socket.player);
-
-    // receive control input from the player: package into JSON and broadcast
-    socket.on('control_input', function(data) {
-      player_input = {
-        id: socket.player.id,
-        up_pressed: data.up_pressed,
-        down_pressed: data.down_pressed,
-        left_pressed: data.left_pressed,
-        right_pressed: data.right_pressed,
-        space_pressed: data.space_pressed
-      };
-      console.log('Received input update from id ' + socket.player.id);
-      socket.broadcast.emit('control_update', player_input);
-      // io.emit('control_update', player_input);
-    });
-
-    // handle a player disconnecting: emit to other connections
-    socket.on('disconnect', function(){
-      io.emit('player_disconnect', socket.player.id);
-    });
+    // add new player to game
+    // returns object with player id, position, etc.
+    game_instance.addPlayer(socket);
   });
 
   socket.on('test', function() {
     console.log('Test received');
   });
 });
-
-function getAllPlayers() {
-  var players = [];
-  Object.keys(io.sockets.connected).forEach(function(socketID) {
-    var player = io.sockets.connected[socketID].player;
-    if (player) {
-      players.push(player);
-    }
-  });
-  return players;
-}
-
-function randomInt(low, high) {
-  return Math.floor(Math.random() * (high - low) + low);
-}
